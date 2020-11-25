a4d913783bf4f58fb5fcec2d7b189703f99d4832
Moravy <moravy22@gmail.com>
2020-11-26 08:34:57 +1300

    refactor code

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+var folder = "./assets/img/gallery";
+
+$.ajax({
+    url : folder,
+    success: function (data) {
+        
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+                console.log((i-6)%6 ),
+                $(".row").append( "<div class='box'><img src='"+ val  +"'/></div>" );
+            } 
+        });
+    }
+});
